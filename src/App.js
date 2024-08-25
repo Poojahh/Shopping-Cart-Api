a908010e4b37b39ab067991adae026c107cedff6
Poojahh <poojakrishnan1104@gmail.com>
2024-08-25 14:23:27 +0530

    Shopping Cart Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import AddProducts from './components/AddProducts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchProducts from './components/SearchProducts';
 
 function App() {
   return (
     <div>
-      <AddProducts/>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<AddProducts/>}/>
+      <Route path="/search" element={<SearchProducts/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
